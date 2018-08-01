8c776ed8910d0b32ac8ad963f285e889b5a34a38
Matt Aimonetti <mattaimonetti@gmail.com>
2018-08-01 12:39:36 -0700

    print all metadata if available

--- cmd/metadata/main.go
+++ cmd/metadata/main.go
@@ -23,13 +23,33 @@ func main() {
 	if err := dec.Err(); err != nil {
 		log.Fatal(err)
 	}
-	if dec.Metadata != nil {
-		fmt.Printf("SampleInfo: %+v\n", dec.Metadata.SamplerInfo)
-		for i, l := range dec.Metadata.SamplerInfo.Loops {
-			fmt.Printf("\tloop [%d]:\t%+v\n", i, l)
-		}
-		for i, c := range dec.Metadata.CuePoints {
-			fmt.Printf("\tcue point [%d]:\t%+v\n", i, c)
-		}
+	if dec.Metadata == nil {
+		fmt.Println("No metadata present")
+		return
+	}
+	fmt.Printf("Artist: %s\n", dec.Metadata.Artist)
+	fmt.Printf("Title: %s\n", dec.Metadata.Title)
+	fmt.Printf("Comments: %s\n", dec.Metadata.Comments)
+	fmt.Printf("Copyright: %s\n", dec.Metadata.Copyright)
+	fmt.Printf("CreationDate: %s\n", dec.Metadata.CreationDate)
+	fmt.Printf("Engineer: %s\n", dec.Metadata.Engineer)
+	fmt.Printf("Technician: %s\n", dec.Metadata.Technician)
+	fmt.Printf("Genre: %s\n", dec.Metadata.Genre)
+	fmt.Printf("Keywords: %s\n", dec.Metadata.Keywords)
+	fmt.Printf("Medium: %s\n", dec.Metadata.Medium)
+	fmt.Printf("Product: %s\n", dec.Metadata.Product)
+	fmt.Printf("Subject: %s\n", dec.Metadata.Subject)
+	fmt.Printf("Software: %s\n", dec.Metadata.Software)
+	fmt.Printf("Source: %s\n", dec.Metadata.Source)
+	fmt.Printf("Location: %s\n", dec.Metadata.Location)
+	fmt.Printf("TrackNbr: %s\n", dec.Metadata.TrackNbr)
+
+	fmt.Println("Sample Info:")
+	fmt.Printf("%+v\n", dec.Metadata.SamplerInfo)
+	for i, l := range dec.Metadata.SamplerInfo.Loops {
+		fmt.Printf("\tloop [%d]:\t%+v\n", i, l)
+	}
+	for i, c := range dec.Metadata.CuePoints {
+		fmt.Printf("\tcue point [%d]:\t%+v\n", i, c)
 	}
 }
